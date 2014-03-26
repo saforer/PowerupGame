dd85df232354607b643966677efe6fdcf2ab3a37
saforer <saforer@gmail.com>
2014-03-26 09:29:56 -0400

    Megermahnz needs boss door gates so let's git this so I can work on my laptop

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerShoot : MonoBehaviour {
-
+public class Gate : MonoBehaviour {
+	GameObject player;
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 
+	void OnCollision2D (Collision2D col) {
 
+	}
 }
